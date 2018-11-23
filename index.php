eaed1611bdd6a11d863963c26610d9a10b5c120e
chzeze <chzeze@qq.com>
2018-11-23 10:20:09 +0800

    updata

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
  *  index.php 入口
  *
  * @copyright			(C) 2015-2030 YANG QQ182860914
- * @license				
+ * @license
+ * author:zeze
+ * updata:2018年11月23日10:18:31
  */
 
 define('RUN_PATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
